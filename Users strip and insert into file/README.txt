743c6b322b2e3a2117624bc10f21015122525116
baigorriajuanp <baigorriajuanp@gmail.com>
2023-02-17 11:21:15 -0300

    02 17 2023 update

---
+++ Users strip and insert into file/README.txt
@@ -0,0 +1,24 @@
+Extract of the job description:
+
+Hi pretty easy python task
+
+so my main document will be like that
+file1.txt :
+@username1
+@username2
+@username3
+@username4
+@username5
+@username6
+@username7
+@username8
+@username9
+@username10
+
+i want output like that
+file2.txt
+@username1 @username2 @username3 /backtoline
+@username4 @username5 @username6 /backtoline
+@username7 @username8 @username9 /backtoline
+
+so each 3 lines on file1.txt document go to 1 line on file2.txt document
---
+++ Users strip and insert into file/script.py
@@ -0,0 +1,31 @@
+def main():
+
+    route_in = r"path of origin file"
+    route_out = r"path of final file"
+    users_list = ""
+    count = 0
+
+    f = open(route_in, "r")
+    for users in f:
+        users = users.strip("\n")
+        users_list = users_list + users + " "    
+
+    f.close()
+
+    lista = users_list.split()
+    print(lista)
+
+    f = open(route_out, "w")
+
+    for i in lista:
+        f.write(i)
+        if count%3 != 2:
+            f.write(" ")
+        else:
+            f.write("\n")
+        count += 1
+
+    f.close()
+  
+if __name__ == "__main__":
+    main()
